93a46b103db86475a4f5c4d5fdfc3d10df16daca
joanita-51 <nakityojoanita51@gmail.com>
2023-12-12 11:18:48 +0300

    Recap of Nodejs day one

---
+++ index.js
@@ -0,0 +1,35 @@
+// JavaScript code illustrating key concepts
+let message = "Hello, Node.js!";
+
+console.log(message)
+
+function greet(name) {
+  return `Greetings, ${name}!`;
+}
+console.log(greet("Anita"))
+
+const numbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+const fs = require('node:fs')
+try {
+    const data = fs.readFileSync('example.txt', 'utf8' )
+    console.log('File content:', data)
+
+} catch (error) {
+    console.error(error)
+}
+
+console.log("Read file synchronously")
+
+const readline = require('node:readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  readline.question(`What's your name?`, name => {
+    console.log(`Hi ${name}!`);
+    readline.close();
+  });
